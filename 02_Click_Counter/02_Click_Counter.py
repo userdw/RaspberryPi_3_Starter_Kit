464b0b4bae86de06ee349d79095e94f0c67e135a
userdw <insidetrainingie@gmail.com>
2017-09-15 10:31:48 +0700

    Update 02_Click_Counter.py

--- 02_Click_Counter/02_Click_Counter.py
+++ 02_Click_Counter/02_Click_Counter.py
@@ -2,12 +2,12 @@ import wiringpi                 # import library wiringPi-Python
 from time import sleep          # import library sleep
 
 wiringpi.wiringPiSetup()        # Must be called before using IO function
-wiringpi.pinMode(1,0)           # set pin 1 to 0 (INPUT)
+wiringpi.pinMode(21,0)           # set pin 2 to 0 (INPUT)
 count = 0
 try:
 while True:                     # endless loop
   # read pin 1, if pin 1 is HUGH (switch presses)
-	inputValue = wiringpi.digitalRead(1)            
+	inputValue = wiringpi.digitalRead(21)            
 	if (inputValue == True):
 		count = count + 1
 		print ("Button pressed " + str(count) + " times")
