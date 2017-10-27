7279dea6dfa3655b26598100232c4c990712ace4
userdw <insidetrainingie@gmail.com>
2017-10-28 04:51:13 +0700

    Update 02_Click_Counter.py

--- 02_Click_Counter/02_Click_Counter.py
+++ 02_Click_Counter/02_Click_Counter.py
@@ -1,16 +1,18 @@
-import wiringpi                 # import library wiringPi-Python
-from time import sleep          # import library sleep
+import wiringpi # import library wiringPi-Python
+from time import sleep # import library sleep
 
-wiringpi.wiringPiSetup()        # Must be called before using IO function
-wiringpi.pinMode(21,0)           # set pin 2 to 0 (INPUT)
+wiringpi.wiringPiSetup() # Must be called before using I/O function
+wiringpi.pinMode(21, 0) # set pin 2 to 0 (INPUT)
 count = 0
+
 try:
-	while True:                     # endless loop
-		# read pin 1, if pin 1 is HIGH (switch presses)
-		inputValue = wiringpi.digitalRead(21)            
-		if (inputValue == True):
+	while True: # endless loop
+		# read pin 1, if pin 1 is HIGH (switch pressed)
+		inputValue = wiringpi.digitalRead(21)
+		if inputValue == True:
 			count = count + 1
-			print ("Button pressed " + str(count) + " times")
-		sleep(.3)
+			print("Button pressed " + str(count) + " times")
+		sleep(0.3)
+		
 except KeyboardInterrupt:
-	print "exit"
+	print("exit")
