1dac6e03f3735071fd29001f06f2618b4cf41bf3
userdw <insidetrainingie@gmail.com>
2017-10-28 05:29:19 +0700

    Update 04_Motion_Detector.py

--- 04_Motion_Detector/04_Motion_Detector.py
+++ 04_Motion_Detector/04_Motion_Detector.py
@@ -1,9 +1,9 @@
 import wiringpi
 from time import sleep
 
-wiringpi.wiringPiSetup() # Must be called before using IO function
-wiringpi.pinMode(1, 0) # Set pin 1 to 0 (INPUT)
-wiringpi.pinMode(21, 1) # Set pin 21 to 1 (OUTPUT)
+wiringpi.wiringPiSetup()
+wiringpi.pinMode(1, 0)
+wiringpi.pinMode(21, 1)
 
 try:
 	while True: # endless loop
