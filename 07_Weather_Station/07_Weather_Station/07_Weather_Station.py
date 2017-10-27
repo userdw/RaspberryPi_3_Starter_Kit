8be0c31e2319eb3718ecb3476d22d5b7ce5101e7
userdw <insidetrainingie@gmail.com>
2017-10-28 05:41:00 +0700

    Update 07_Weather_Station.py

--- 07_Weather_Station/07_Weather_Station/07_Weather_Station.py
+++ 07_Weather_Station/07_Weather_Station/07_Weather_Station.py
@@ -4,20 +4,18 @@ import datetime
 import os
 from time import strftime
 
-
 try:
-  while 1:
-    	os.system('clear')
-	value1= MCP3202.readADC(0)	# range data 0 - vref (volt)
-	voltage=round(float(value1*5000/4096),2)
-	temperature = (voltage-550)/10
-	tampil= round(float(temperature),2)
- 	print "Weather Station"
-	print "Curent Temperature : ",tampil,u"\u2103"
-    	print ""
-	print "Press CTRL+C to exit"
-   	time.sleep(0.075)
+	while 1:
+    		os.system("clear")
+		value1= MCP3202.readADC(0)
+		voltage = round(float(value1 * 5000 / 4096), 2)
+		temperature = (voltage - 550) / 10
+		tampil = round(float(temperature), 2)
+ 		print("Weather Station")
+		print("Curent Temperature : ", tampil, u"\xb0", "C")
+    		print("")
+		print("Press CTRL+C to exit")
+   		time.sleep(0.075)
 
 except KeyboardInterrupt:
-    	print "exit"
-
+	print("exit")
