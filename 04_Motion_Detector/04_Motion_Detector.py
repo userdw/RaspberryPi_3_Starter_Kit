20a93dbfb95c14626a61425f02801a1d6eb7fba8
userdw <insidetrainingie@gmail.com>
2017-10-28 05:03:10 +0700

    Update 04_Motion_Detector.py

--- 04_Motion_Detector/04_Motion_Detector.py
+++ 04_Motion_Detector/04_Motion_Detector.py
@@ -1,21 +1,22 @@
-import wiringpi
-from time import sleep                         # import library WiringPi-Python
+import wiringpi # import library WiringPi-Python
+from time import sleep
 
-wiringpi.wiringPiSetup()                       # Must be called before using IO function
-wiringpi.pinMode(1,0)                          # Set pin 1 to 0 (INPUT)
-wiringpi.pinMode(21,1)                         # Set pin 21 to 1 (OUTPUT)
+wiringpi.wiringPiSetup() # Must be called before using IO function
+wiringpi.pinMode(1, 0) # Set pin 1 to 0 (INPUT)
+wiringpi.pinMode(21, 1) # Set pin 21 to 1 (OUTPUT)
 
 try:
-	while True:                                   # endless loop
-		if wiringpi.digitalRead(1)== 1:        # read pin 1, if pin 1 is 0 (LOW)
-			print "Moving Object Detected"
-			#sleep(0.5)
-			wiringpi.digitalWrite(21,1)     # if True, write pin 21 to 1 (HIGH)
+	while True: # endless loop
+		if wiringpi.digitalRead(1) == True:
+			print("Moving Object Detected")
+			sleep(0.5)
+			wiringpi.digitalWrite(21, 1) # if True, write pin 21 to 1 (HIGH)
 		else:
-			print "No Object Detected"
-			#sleep(0.5)
-			wiringpi.digitalWrite(21,0)     # if False, write pin 21 to 0 (LOW)
+			print("No Object Detected")
+			sleep(0.5)
+			wiringpi.digitalWrite(21, 0) # if False, write pin 21 to 0 (LOW)
 			
 except KeyboardInterrupt:
-	wiringpi.softPwmWrite(21,0)		# PWM pulse on pin 21
-	print "exit"
+	wiringpi.pinMode(21, 1)
+	wiringpi.digitalWrite(21, 0)
+	print("exit")
