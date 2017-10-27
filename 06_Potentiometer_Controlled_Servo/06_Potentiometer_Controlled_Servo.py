c760a0c8b06367d5e2ca954cb94d47efc2fcbe61
userdw <insidetrainingie@gmail.com>
2017-10-28 05:17:04 +0700

    Update 06_Potentiometer_Controlled_Servo.py

--- 06_Potentiometer_Controlled_Servo/06_Potentiometer_Controlled_Servo.py
+++ 06_Potentiometer_Controlled_Servo/06_Potentiometer_Controlled_Servo.py
@@ -1,8 +1,8 @@
-import MCP3202, wiringpi,os		# import library WiringPi2-Python
+import MCP3202, wiringpi, os
 from time import sleep
 
-wiringpi.wiringPiSetup()		# Must be called before using IO function
-wiringpi.softPwmCreate(1,0,100)		
+wiringpi.wiringPiSetup() # Must be called before using I/O function
+wiringpi.softPwmCreate(1, 0, 100)		
 
 def translate(value,leftMin,leftMax,rightMin,rightMax):
 	# Figure out how 'wide' each range is
@@ -12,20 +12,21 @@ def translate(value,leftMin,leftMax,rightMin,rightMax):
     	valueScaled = float(value - leftMin) / float(leftSpan)
     	# Convert the 0-1 range into a value in the right range.
     	return rightMin + (valueScaled * rightSpan)
+
 try:
- while 1:					# endless loop
-	os.system('clear')
-	value1 = MCP3202.readADC(0)		# range data 0 - vref (volt)
-	map = translate(value1,0,4095,0,100)
-	position = translate (map,100,0,0,180)
- 	print "Servo Position"
-	print "Curent Position : ",int(position),"degree"
-    	print ""
-	print "Press CTRL+C to exit"
-	wiringpi.softPwmWrite(1,int(map))	# PWM pulse on pin 9
-	sleep(0.001)
+	while 1: # endless loop
+		os.system("clear")
+		value1 = MCP3202.readADC(0)
+		map = translate(value1, 0, 4095, 0, 100)
+		position = translate (map, 100, 0, 0, 180)
+ 		print("Servo Position")
+		print("Curent Position : ", int(position), "degree")
+    		print("")
+		print("Press CTRL+C to exit")
+		wiringpi.softPwmWrite(1, int(map))
+		sleep(0.001)
 
 except KeyboardInterrupt:
-	wiringpi.softPwmWrite(1,0)		# PWM pulse on pin 9
+	wiringpi.softPwmWrite(1, 0)
 	print "exit"	
 
