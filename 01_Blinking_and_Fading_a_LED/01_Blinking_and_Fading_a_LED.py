24552df73caa8e34ffbab09e6435173d0efe747e
userdw <insidetrainingie@gmail.com>
2017-11-09 13:17:36 +0700

    Add files via upload

--- 01_Blinking_and_Fading_a_LED/01_Blinking_and_Fading_a_LED.py
+++ 01_Blinking_and_Fading_a_LED/01_Blinking_and_Fading_a_LED.py
@@ -1,34 +1,35 @@
-import wiringpi # import WiringPi-Python library
-from time import sleep # import sleep library
+import wiringpi as wpi
+from time import sleep
 
 LED_MODULE = 1
-wiringpi.wiringPiSetup() # Must be called before using I/O function
+
+wpi.wiringPiSetup() # Must be called before calling any I/O function
 
 def toogleLED(times):
-	wiringpi.pinMode(LED_MODULE, 1) # Set pin LED_MODULE to 1 (OUTPUT) / 0 (INPUT)
+	wpi.pinMode(LED_MODULE, wpi.OUTPUT)
 	for i in range(0, times, 1):
 		print("on")
-		wiringpi.digitalWrite(LED_MODULE, 1) # set LED_MODULE to high
+		wpi.digitalWrite(LED_MODULE, wpi.HIGH)
 		sleep(1)
 		print("off")
-		wiringpi.digitalWrite(LED_MODULE, 0) # set LED_MODULE to low
+		wpi.digitalWrite(LED_MODULE, wpi.LOW)
 		sleep(1)
 		
 def fadeLED(times):
 	MIN_PWM = 0
 	MAX_PWM = 100
-	wiringpi.softPwmCreate(LED_MODULE, MIN_PWM, MAX_PWM) # Set PWM on pin LED_MODULE with min value = 0 and max value = 100
+	wpi.softPwmCreate(LED_MODULE, MIN_PWM, MAX_PWM) # Set PWM on pin LED_MODULE with initial value = MIN_PWM and range = 100
 	
 	for i in range(0, times, 1):
 		for j in range(MIN_PWM, (MAX_PWM + 1), 1):
-			wiringpi.softPwmWrite(LED_MODULE, j) # PWM pulse on pin LED_MODULE
+			wpi.softPwmWrite(LED_MODULE, j)
 			sleep(0.02)
 		for j in range(MAX_PWM, (MIN_PWM - 1), -1):
-			wiringpi.softPwmWrite(LED_MODULE, j) # PWM pulse on pin LED_MODULE
+			wpi.softPwmWrite(LED_MODULE, j)
 			sleep(0.02)
 			
 try:
-	while True: # endless loop
+	while True:
 		print("Start")
 		toogleLED(3)
 		sleep(2)
@@ -36,6 +37,6 @@ try:
 		sleep(1)
 	
 except KeyboardInterrupt :
-	wiringpi.pinMode(LED_MODULE, 1)
-	wiringpi.digitalWrite(LED_MODULE, 0)
+	wpi.pinMode(LED_MODULE, wpi.OUTPUT)
+	wpi.digitalWrite(LED_MODULE, wpi.LOW)
 	print("exit")
