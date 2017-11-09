fec32738418cdc17bea1868217c98a9b1e0afb4b
userdw <insidetrainingie@gmail.com>
2017-11-09 10:18:37 +0700

    Create 07_Weather_Station_python3.py

---
+++ 07_Weather_Station/07_Weather_Station/07_Weather_Station_python3.py
@@ -0,0 +1,22 @@
+import MCP3202_python3
+import time
+import datetime
+import os
+from time import strftime
+
+try:
+  while 1:
+    os.system("clear")
+    value1= MCP3202.readADC(0)
+    voltage = round(float(value1 * 5000 / 4096), 2)
+    temperature = (voltage - 550) / 10
+    tampil = round(float(temperature), 2)
+    print("Weather Station")
+    print( "Curent Temperature : ", tampil, u"\xb0", "C")
+    print("")
+    print("Press CTRL+C to exit")
+    time.sleep(0.075)
+
+except KeyboardInterrupt:
+    print("exit")
+
